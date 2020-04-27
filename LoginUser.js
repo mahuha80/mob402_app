3d50983fefadf9e885cf1aa0d38fcbd741aa570b
mahuha80 <dracula01112000@gmail.com>
2020-04-27 16:46:59 +0700

    register

--- LoginUser.js
+++ LoginUser.js
@@ -1,10 +1,14 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 const API = "192.168.0.104:3000/api/user/login";
 
 export default function LoginUser({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [resLogin, setResLogin] = useState("");
+
   return (
     <View style={styles.container}>
       <View
@@ -18,7 +22,14 @@ export default function LoginUser({ navigation }) {
       >
         <Text style={{ marginRight: 10 }}>Username</Text>
         <TextInput
-          style={{ borderWidth: 1, borderColor: "black", width: 300 }}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+          style={{
+            borderWidth: 1,
+            borderColor: "black",
+            width: 300,
+            paddingLeft: 10,
+          }}
         />
       </View>
       <View
@@ -32,25 +43,77 @@ export default function LoginUser({ navigation }) {
       >
         <Text style={{ marginRight: 10 }}>Password</Text>
         <TextInput
-          style={{ borderWidth: 1, borderColor: "black", width: 300 }}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          value={password}
+          style={{
+            borderWidth: 1,
+            borderColor: "black",
+            width: 300,
+            paddingLeft: 10,
+          }}
         />
       </View>
-      <View style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'stretch'}}>
-      <Button
-        style={{}}
-        title="Login"
-        onPress={() => {
-          navigation.navigate("Home");
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "stretch",
         }}
-      />
-      <Text>          </Text>
-      <Button
-        style={{ }}
-        title="Register"
-        onPress={() => {
-          navigation.navigate("Register");
-        }}
-      />
+      >
+        <Button
+          style={{}}
+          title="Login"
+          onPress={() => {
+            if (
+              username.length == 0 ||
+              password.length == 0 ||
+              username.length < 5 ||
+              password.length < 5
+            ) {
+              Alert.alert("Vui lòng nhập đầy đủ các trường");
+              return false;
+            } else {
+              fetch("http://192.168.0.104:3000/api/user/login", {
+                method: "POST",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  username: `${username}`,
+                  password: `${password}`,
+                }),
+              })
+                .then((response) => response.json())
+                .then((json) => setResLogin(json));
+              if (resLogin.length == 0) {
+                Alert.alert("Waiting ...");
+              } else {
+                if (resLogin.token == "" && resLogin.username == "") {
+                  Alert.alert("Sai tài khoản hoặc mật khẩu");
+                  setResLogin("");
+                  setUsername("");
+                  setPassword("");
+                } else {
+                  setResLogin("");
+                  setUsername("");
+                  setPassword("");
+                  navigation.navigate("Home");
+                }
+              }
+            }
+          }}
+        />
+        <Text> </Text>
+        <Button
+          style={{}}
+          title="Register"
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        />
       </View>
     </View>
   );
--- RegisterUser.js
+++ RegisterUser.js
@@ -1,58 +1,129 @@
-import React from "react";
-import { StyleSheet, Text, View ,TextInput,Button} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
-const API = '192.168.0.104:3000/api/user/login';
+const API = "192.168.0.104:3000/api/user/login";
 
-export default function RegisterUser({navigation}) {
+export default function RegisterUser({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [retype, setRetype] = useState("");
+  const [resRegister, setResRegister] = useState();
+  fetchData = async () => {
+    if (
+      username.length == 0 ||
+      password.length == 0 ||
+      retype.length == 0 ||
+      username.length < 5 ||
+      password.length < 5 ||
+      retype.length < 5
+    ) {
+      Alert.alert("Vui lòng nhập đầy đủ các trường");
+    } else if (password !== retype) {
+      Alert.alert("Vui lòng nhập mật khẩu trùng nhau");
+    } else {
+      fetch("http://192.168.0.104:3000/api/user/register", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: `${username}`,
+          password: `${password}`,
+        }),
+      })
+        .then((response) => response.json())
+        .then((json) => setResRegister(json));
+      try {
+        if (resRegister.status === "failed") {
+          setUsername("");
+          setPassword("");
+          setResRegister("");
+          setRetype("");
+          Alert.alert(resRegister.msg);
+        } else {
+          setUsername("");
+          setPassword("");
+          setResRegister("");
+          setRetype("");
+          Alert.alert(resRegister.status);
+          navigation.navigate("LoginUser");
+        }
+      } catch (error) {
+        if (error) throw error;
+      }
+    }
+  };
   return (
-      <View style={styles.container}>
-        <View
+    <View style={styles.container}>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+        }}
+      >
+        <Text style={{ marginRight: 20 }}>Username</Text>
+        <TextInput
+          value={username}
+          onChangeText={(text) => setUsername(text)}
           style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: 20,
+            borderWidth: 1,
+            borderColor: "black",
+            width: 300,
+            paddingLeft: 10,
           }}
-        >
-          <Text style={{ marginRight: 20 }}>Username</Text>
-          <TextInput
-            style={{ borderWidth: 1, borderColor: "black", width: 300 }}
-          />
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: 20,
-          }}
-        >
-          <Text style={{ marginRight: 20 }}>Password</Text>
-          <TextInput
-            style={{ borderWidth: 1, borderColor: "black", width: 300 }}
-          />
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: 20,
-          }}
-        >
-          <Text style={{ marginRight: 20 }}>Retype     </Text>
-          <TextInput
-            style={{ borderWidth: 1, borderColor: "black", width: 300 }}
-          />
-        </View>
-        <Button title="Register" onPress={() => {
-          navigation.navigate('Home')
-        }} />
+        />
       </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+        }}
+      >
+        <Text style={{ marginRight: 20 }}>Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={{
+            borderWidth: 1,
+            borderColor: "black",
+            width: 300,
+            paddingLeft: 10,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+        }}
+      >
+        <Text style={{ marginRight: 20 }}> Retype </Text>
+        <TextInput
+          value={retype}
+          secureTextEntry={true}
+          onChangeText={(text) => setRetype(text)}
+          style={{
+            borderWidth: 1,
+            borderColor: "black",
+            width: 300,
+            paddingLeft: 10,
+          }}
+        />
+      </View>
+      <Button title="Register" onPress={this.fetchData()} />
+    </View>
   );
 }
 

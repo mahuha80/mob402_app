b67378aba50d8138d74e0f31bf0aa76816c0298a
mahuha80 <dracula01112000@gmail.com>
2020-04-27 23:00:11 +0700

    login register show product'

--- LoginUser.js
+++ LoginUser.js
@@ -10,27 +10,44 @@ export default function LoginUser({ navigation }) {
   const [resLogin, setResLogin] = useState("");
   async function postLogin() {
     {
-        let response = await fetch("http://192.168.0.104:3000/api/user/login", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: `${username}`,
-            password: `${password}`,
-          }),
-        });
-        let json = await response.json();
-        setResLogin(json)   
-        if (resLogin.username == "") {
-          Alert.alert("Sai tài khoản hoặc mật khẩu");
-        } else {
-          navigation.navigate("Home");
-        }
-      
+      let response = await fetch("http://192.168.0.104:3000/api/user/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: `${username}`,
+          password: `${password}`,
+        }),
+      });
+      let json = await response.json();
+      setResLogin(json);
+      if (resLogin.username == "") {
+        setUsername("");
+        setPassword("");
+        setResLogin("");
+        Alert.alert("Sai tài khoản hoặc mật khẩu");
+      } else {
+        setUsername("");
+        setPassword("");
+        setResLogin("");
+        navigation.navigate("Home");
+      }
     }
   }
+const showButton = ()=> {
+    if (
+      username.length == 0 ||
+      password.length == 0 ||
+      username.length < 5 ||
+      password.length < 5
+    ) {
+      Alert.alert("Vui lòng nhập đầy đủ các trường");
+    } else {
+      postLogin();
+  }
+};
   return (
     <View style={styles.container}>
       <View
@@ -44,6 +61,7 @@ export default function LoginUser({ navigation }) {
       >
         <Text style={{ marginRight: 10 }}>Username</Text>
         <TextInput
+          value={username}
           onChangeText={(text) => setUsername(text)}
           style={{
             borderWidth: 1,
@@ -64,6 +82,7 @@ export default function LoginUser({ navigation }) {
       >
         <Text style={{ marginRight: 10 }}>Password</Text>
         <TextInput
+          value={password}
           onChangeText={(txt) => setPassword(txt)}
           secureTextEntry={true}
           style={{
@@ -85,18 +104,7 @@ export default function LoginUser({ navigation }) {
         <Button
           style={{}}
           title="Login"
-          onPress={() => {
-            if (
-              username.length == 0 ||
-              password.length == 0 ||
-              username.length < 5 ||
-              password.length < 5
-            ) {
-              Alert.alert("Vui lòng nhập đầy đủ các trường");
-            } else {
-              postLogin();
-            }
-          }}
+          onPress={() =>showButton()}
         />
         <Text> </Text>
         <Button
--- Product.js
+++ Product.js
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-const Tab = createBottomTabNavigator();
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  Image,
+} from "react-native";
+const productAPI = "http://192.168.0.104:3000/api/product/getAllProducts";
+const base = "http://192.168.0.104:3000/";
+async function getProductFromAPI() {
+  try {
+    let response = await fetch(productAPI);
+    let json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-export default function Product() {
-  const productAPI ='http://192.168.0.104:3000/api/product/getAllProducts';
+function Item({ image ,name,price,type}) {
   return (
-    <View style={styles.container}>
-      <Text>Product AAAAA</Text>
+    <View style={styles.item}>
+      <Image
+        source={{ uri: base + image }}
+        style={{ width: 200, height: 100 }}
+      ></Image>
+      <Text>{name}</Text>
+      <Text>{price}</Text>
+      <Text>{type}</Text>
+
     </View>
   );
 }
 
-
+export default function Product() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    getProductFromAPI().then((data) => setData(data));
+  });
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item image={item.image} name={item.name} price={item.price} type={item.type} />}
+        keyExtractor={(item) => item._id}
+      />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
